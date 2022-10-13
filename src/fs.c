d4dcf9c394013495eb711e16bde76687c241c720
lucasvmx <lucas.engen@hotmail.com>
2022-10-13 00:01:52 -0300

    refactor: :recycle: prevents use after free and removes useless code
    Signed-off-by: lucasvmx <lucas.engen@hotmail.com>

--- src/fs.c
+++ src/fs.c
@@ -33,40 +33,16 @@ static EVMFD_FILE *evmfd_file_init(__u_long size, __u_short binary)
 
 void evmfd_file_close(EVMFD_FILE **f)
 {
-    free((*f)->data);
+    if((*f)->data != NULL) {
+        free((*f)->data);
+        (*f)->data = NULL;
+    } else {
+        free((*f)->bin_data);
+        (*f)->bin_data = NULL;
+    }
+
     free(*f);
-}
-
-EVMFD_FILE *evmfd_read_text_file(const char *filename)
-{
-    int fd = -1;
-    struct stat file_info;
-    EVMFD_FILE *file = NULL;
-
-    assert(stat(filename, &file_info) == 0);
-
-    fd = open(filename, O_RDONLY);
-    assert(fd != -1);
-
-    file = evmfd_file_init(file_info.st_size, TEXT);
-    
-    int n = 0, index = 0;
-    char chunk[1024];
-
-    // Lê o arquivo em partes pequenas
-    do {
-        n = read(fd, &chunk, sizeof(chunk));
-        if(n) {
-            memcpy(&file->data[index], chunk, n);
-            index += n;
-        }
-    } while(n > 0);
-
-    file->data_size = index;
-
-    close(fd);
-
-    return file;
+    (*f) = NULL;
 }
 
 EVMFD_FILE *evmfd_read_bin_file(const char *filename)
@@ -97,38 +73,6 @@ EVMFD_FILE *evmfd_read_bin_file(const char *filename)
     file->data_size = index;
 
     close(fd);
-
-    return file;
-}
-
-EVMFD_FILE *evmfd_read_bin_file_h(const char *filename)
-{
-    FILE *fd;
-    struct stat file_info;
-    EVMFD_FILE *file = NULL;
-
-    assert(stat(filename, &file_info) == 0);
-
-    fd = fopen(filename, "rb");
-    assert(fd != NULL);
-
-    file = evmfd_file_init(file_info.st_size, BINARY);
-    
-    int n = 0, index = 0;
-    unsigned char chunk[1024];
-
-    // Lê o arquivo em partes pequenas
-    do {
-        n = fread(chunk, sizeof(unsigned char), sizeof(chunk),fd);
-        if(n > 0) {
-            memcpy(&file->bin_data[index], chunk, n);
-            index += n;
-        }
-    } while(n > 0);
-
-    file->data_size = index;
-
-    fclose(fd);
 
     return file;
 }
--- src/fs.h
+++ src/fs.h
@@ -9,9 +9,7 @@ typedef struct evmfd_file {
     __u_long data_size;
 } EVMFD_FILE;
 
-EVMFD_FILE *evmfd_read_text_file(const char *filename);
 EVMFD_FILE *evmfd_read_bin_file(const char *filename);
-EVMFD_FILE *evmfd_read_bin_file_h(const char *filename);
 void evmfd_file_close(EVMFD_FILE **f);
 
 #endif
